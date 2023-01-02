c521b61276a722bb0a7e1d0aad5eaa9b3bbd6628
Amresh Yadav <ayadav.1918@gmail.com>
2023-01-02 21:00:02 +0530

    Post Display on Home Page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,23 @@
+const Post = require('../models/post');
 module.exports.home = function(req,res){
 console.log(req.cookies);
+//res.cookie('User_Id',25);
 
-res.cookie('User_Id',25);
+// Post.find({},function(err, posts){
+//     return res.render('home', {
+//         title: "Codeial | Home",
+//         posts: posts
+//         });
+// })
 
+
+
+//populating the user of each post
+Post.find({}).populate('user').exec(function(err, posts){
     return res.render('home', {
-        title: "Home"
-    });
+        title: "Codeial | Home",
+        posts: posts
+        });
+});
+
 }
